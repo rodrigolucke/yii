ed9de0c13ab0f4927b95996335149d79f207a4ea
rodrigolucke <digolucke@gmail.com>
2017-11-08 08:31:33 -0200

    Update web.php

--- config/web.php
+++ config/web.php
@@ -56,7 +56,7 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
+    // configuration adjustments for 'dev' environment.
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
