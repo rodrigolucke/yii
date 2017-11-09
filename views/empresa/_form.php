03caa4ae179e8e6618b6232a5dbd52ba4aa0c3cc
teste <teste@teste-PC>
2017-11-09 00:21:32 -0200

    ARRUMADO ACESSO EMPRSA CREATE

--- views/empresa/_form.php
+++ views/empresa/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper; 
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Empresa */
@@ -15,9 +16,11 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'NOME_EMPRESA')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'CNPJ')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'COD_ENDERECO')->textInput() ?>
-
+  
+    <?= $form->field($model, 'COD_ENDERECO')->dropDownList(ArrayHelper::map(\app\models\Endereco::find()->all(), 'COD_ENDERECO', 'RUA'), ['prompt'=>'']) ?>
+    <p>
+        <?= Html::a('Cadastrar Endereço', ['endereco/create'], ['class' => 'btn btn-info']) ?>
+    </p>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
