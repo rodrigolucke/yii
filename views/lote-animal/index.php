89cc3478847ab90cb9ca80c9416a0bd61b38f609
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-10 18:51:45 -0200

    Update index.php

--- views/lote-animal/index.php
+++ views/lote-animal/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Lote Animals';
+$this->title = 'Lote Animais';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="lote-animal-index">
