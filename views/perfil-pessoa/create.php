86a706538b2857c6fc46f74c88472bd315a5c255
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-10 11:57:14 -0200

    Update create.php

--- views/perfil-pessoa/create.php
+++ views/perfil-pessoa/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\PerfilPessoa */
 
-$this->title = 'Create Perfil Pessoa';
+$this->title = 'Incluir Perfil Pessoa';
 $this->params['breadcrumbs'][] = ['label' => 'Perfil Pessoas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
