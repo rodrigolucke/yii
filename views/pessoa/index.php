6ef8cc533cdc6ba896092c269872e4114f2386ee
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-10 11:53:26 -0200

    Update index.php

--- views/pessoa/index.php
+++ views/pessoa/index.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Pessoa', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Incluir Pessoa', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
