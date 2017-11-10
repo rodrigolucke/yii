9d5fdce941a4b90c7b348da18c519df5ac4a72ac
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-10 18:40:53 -0200

    Update index.php

--- views/medicacao/index.php
+++ views/medicacao/index.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Medicacao', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Incluir Medicacao', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
