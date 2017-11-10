646580015fc47e5304b649abc859c07bb738025d
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-10 11:53:04 -0200

    Update create.php

--- views/pessoa/create.php
+++ views/pessoa/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Pessoa */
 
-$this->title = 'Create Pessoa';
+$this->title = 'Incluir Pessoa';
 $this->params['breadcrumbs'][] = ['label' => 'Pessoas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
