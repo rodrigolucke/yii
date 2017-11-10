18c662b17045611536e7b820490b2dde61072be8
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-10 11:55:33 -0200

    Update update.php

--- views/perfil-menu/update.php
+++ views/perfil-menu/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\PerfilMenu */
 
-$this->title = 'Update Perfil Menu: ' . $model->SEQ_PERFIL_MENU;
+$this->title = 'Editar Perfil Menu: ' . $model->SEQ_PERFIL_MENU;
 $this->params['breadcrumbs'][] = ['label' => 'Perfil Menus', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->SEQ_PERFIL_MENU, 'url' => ['view', 'id' => $model->SEQ_PERFIL_MENU]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Editar';
 ?>
 <div class="perfil-menu-update">
 
