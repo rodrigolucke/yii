8bd93755f4492263e4389a79b07031064b954475
geisagiovana <geisa.giovana@yahoo.com.br>
2017-11-25 16:50:25 -0200

    alterado para portugues

--- views/venda/_form.php
+++ views/venda/_form.php
@@ -25,7 +25,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'COD_MOVIMENTO_ESTOQUE')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Salvar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
